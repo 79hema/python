c9d279adbafe510c0e62e080a0fec26fc55497ef
hemalatha c n <32951379+79hema@users.noreply.github.com>
2017-10-21 14:20:21 +0530

    Add files via upload

---
+++ mod4.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse012
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse012 2017
+# Licence:     <your licence>
+
+def main():
+    print ("hello world")
+float    num1 = 1.5
+float = 6.3
+sum = float(num1) + float(num2)
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+if __name__ == '__main__':
+    main()
