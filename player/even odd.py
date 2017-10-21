f380fe87a1f44f5b82c95d3f126fb0d7bb15ac30
hemalatha c n <32951379+79hema@users.noreply.github.com>
2017-10-21 15:59:44 +0530

    Add files via upload

---
+++ player/even odd.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse012
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse012 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+ def function():
+    print("odd or even")
+v=int(input("please enter the num"))
+if(v%2==0):
+ print("v is even")
+else:
+      print("v is odd")
+      function()
