bdeeb64528c75caf1e45d754b43fca9e551cbe06
hemalatha c n <32951379+79hema@users.noreply.github.com>
2017-10-21 14:19:36 +0530

    Add files via upload

---
+++ variable oper.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      16cse012
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse012 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    a=10
+    b=20
+    print(a+b)
+    print(a-b)
+    print(a**b)
+    print(a/b)
+    print(a%b)
+    print(a==b)
+    print(a<>b)
+
+if __name__ == '__main__':
+    main()
+C:\Users\16cse012\Documents\variable oper.py
