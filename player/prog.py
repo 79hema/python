0dc894f422e5db4696d9ad43ee10eee629c9cdd6
hemalatha c n <32951379+79hema@users.noreply.github.com>
2017-10-21 14:18:56 +0530

    Add files via upload

--- player/prog.py
+++ player/prog.py
@@ -1,1 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse012
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse012 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
 
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+stringvar="programmer"
+a=stringvar.count("programmer")
+print(a)
+a=stringvar.find("g")
+print(a)
+a=stringvar.replace('m,','y')
+print(a)
